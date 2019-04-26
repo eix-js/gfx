3e2f93ff3f8a4c726b591d7022e987851e883100
Stepan Shabalin <stomperhomp@gmail.com>
2019-04-26 22:06:33 +0700

    added test base

---
+++ test/renderingJobs/sprite.ts
@@ -0,0 +1,8 @@
+import { spriteRenderingJob } from "../../src/main"
+import { ECS, JobSystem } from "@eix/core"
+
+describe("Test sprite", () => {
+    spriteRenderingJob
+    ECS
+    JobSystem
+})
