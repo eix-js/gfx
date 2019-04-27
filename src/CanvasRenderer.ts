71ad94b48dcfb0bd7adc35080c80eeb5f46bf42b
Stepan Shabalin <stomperhomp@gmail.com>
2019-04-27 07:06:00 +0700

    made CanvasRenderer work

--- src/CanvasRenderer.ts
+++ src/CanvasRenderer.ts
@@ -2,7 +2,12 @@ import { vec2 } from "gl-matrix"
 import { spriteComponent } from "./renderingJobs/sprite"
 
 export class CanvasRenderer {
+    private ctx: CanvasRenderingContext2D
+    constructor(private canvas: HTMLCanvasElement) {
+        this.ctx = canvas.getContext("2d")
+    }
+
     drawImage(id: number, image: spriteComponent, position: vec2): void {
-        // TODO
+        this.ctx.drawImage(image.image, position[0], position[1])
     }
 }
