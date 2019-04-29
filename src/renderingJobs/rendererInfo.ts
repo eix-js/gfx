a2f3efa84660151b0b0ebe312aa0594bf82f37c6
Stepan Shabalin <46641404+neverix@users.noreply.github.com>
2019-04-29 07:18:37 +0700

    changed comments on rendererInfo

--- src/renderingJobs/rendererInfo.ts
+++ src/renderingJobs/rendererInfo.ts
@@ -2,11 +2,11 @@ import { Renderer } from "../interfaces/Renderer"
 import { ECS, idKey } from "@eix/core"
 
 /**
- * sprite rendering job. renders entities with the spriteComponent component.
+ * renderer info job. adds information about the renderer to ECS
  */
 export const rendererInfoJob = ([env, backend]: [ECS, Renderer]) => {
     return () => {
-        // add a new entity with the specified component
+        // add a new entity with the renderer info
         env.addEntityFlowGroup().addComponent("rendererInfo", backend.rendererInfo)
     }
 }
